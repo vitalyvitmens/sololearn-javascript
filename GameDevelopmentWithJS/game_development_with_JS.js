44c8e5c84430f6d043c84df05e928cfa82ed5a68
vitalyvitmens <otm_@tut.by>
2022-12-12 11:20:24 +0300

    added data in files

--- GameDevelopmentWithJS/game_development_with_JS.js
+++ GameDevelopmentWithJS/game_development_with_JS.js
@@ -2234,129 +2234,129 @@ Your browser does not support the HTML5 canvas tag.
 // В качестве завершающего шага вместо значения FPS покажем текущий счет:
 // Это делает полнофункциональную игру, в которой вы собираете монеты и увеличиваете свой счет.
 // Запустите код, чтобы проверить это!
-// window.onload = function () {
-//   var canvas = document.getElementById('canvas')
-//   var context = canvas.getContext('2d')
-//   var x = 250
-//   var y = 150
-//   var coinx = Math.random() * (600 - 50)
-//   var coiny = Math.random() * (400 - 50)
+window.onload = function () {
+  var canvas = document.getElementById('canvas')
+  var context = canvas.getContext('2d')
+  var x = 250
+  var y = 150
+  var coinx = Math.random() * (600 - 50)
+  var coiny = Math.random() * (400 - 50)
 
-//   var t = Date.now()
-//   let speed = 300
-//   let dir = 0
-//   let score = 0
+  var t = Date.now()
+  let speed = 300
+  let dir = 0
+  let score = 0
 
-//   let up = document.getElementById('up')
-//   let down = document.getElementById('down')
-//   let left = document.getElementById('left')
-//   let right = document.getElementById('right')
+  let up = document.getElementById('up')
+  let down = document.getElementById('down')
+  let left = document.getElementById('left')
+  let right = document.getElementById('right')
 
-//   up.onmousedown = function () {
-//     dir = 4
-//   }
-//   down.onmousedown = function () {
-//     dir = 3
-//   }
-//   left.onmousedown = function () {
-//     dir = 2
-//   }
-//   right.onmousedown = function () {
-//     dir = 1
-//   }
+  up.onmousedown = function () {
+    dir = 4
+  }
+  down.onmousedown = function () {
+    dir = 3
+  }
+  left.onmousedown = function () {
+    dir = 2
+  }
+  right.onmousedown = function () {
+    dir = 1
+  }
 
-//   up.ontouchstart = function () {
-//     dir = 4
-//   }
-//   down.ontouchstart = function () {
-//     dir = 3
-//   }
-//   left.ontouchstart = function () {
-//     dir = 2
-//   }
-//   right.ontouchstart = function () {
-//     dir = 1
-//   }
+  up.ontouchstart = function () {
+    dir = 4
+  }
+  down.ontouchstart = function () {
+    dir = 3
+  }
+  left.ontouchstart = function () {
+    dir = 2
+  }
+  right.ontouchstart = function () {
+    dir = 1
+  }
 
-//   up.onmouseup = function () {
-//     dir = 0
-//   }
-//   down.onmouseup = function () {
-//     dir = 0
-//   }
-//   left.onmouseup = function () {
-//     dir = 0
-//   }
-//   right.onmouseup = function () {
-//     dir = 0
-//   }
+  up.onmouseup = function () {
+    dir = 0
+  }
+  down.onmouseup = function () {
+    dir = 0
+  }
+  left.onmouseup = function () {
+    dir = 0
+  }
+  right.onmouseup = function () {
+    dir = 0
+  }
 
-//   up.ontouchend = function () {
-//     dir = 0
-//   }
-//   down.ontouchend = function () {
-//     dir = 0
-//   }
-//   left.ontouchend = function () {
-//     dir = 0
-//   }
-//   right.ontouchend = function () {
-//     dir = 0
-//   }
+  up.ontouchend = function () {
+    dir = 0
+  }
+  down.ontouchend = function () {
+    dir = 0
+  }
+  left.ontouchend = function () {
+    dir = 0
+  }
+  right.ontouchend = function () {
+    dir = 0
+  }
 
-//   function draw() {
-//     var timePassed = (Date.now() - t) / 1000
-//     t = Date.now()
+  function draw() {
+    var timePassed = (Date.now() - t) / 1000
+    t = Date.now()
 
-//     context.clearRect(0, 0, 600, 400)
+    context.clearRect(0, 0, 600, 400)
 
-//     context.font = '25px Arial'
-//     context.fillStyle = 'black'
-//     context.fillText('Score: ' + score, 20, 30)
+    context.font = '25px Arial'
+    context.fillStyle = 'black'
+    context.fillText('Score: ' + score, 20, 30)
 
-//     context.beginPath()
-//     context.rect(x, y, 100, 100)
-//     context.fillStyle = 'red'
-//     context.fill()
+    context.beginPath()
+    context.rect(x, y, 100, 100)
+    context.fillStyle = 'red'
+    context.fill()
 
-//     context.beginPath()
-//     context.rect(coinx, coiny, 50, 50)
-//     context.fillStyle = '#e3c228'
-//     context.fill()
+    context.beginPath()
+    context.rect(coinx, coiny, 50, 50)
+    context.fillStyle = '#e3c228'
+    context.fill()
 
-//     if (dir == 1) {
-//       if (x + 100 < 600) {
-//         x += speed * timePassed
-//       }
-//     } else if (dir == 2) {
-//       if (x > 0) {
-//         x -= speed * timePassed
-//       }
-//     } else if (dir == 3) {
-//       if (y + 100 < 400) {
-//         y += speed * timePassed
-//       }
-//     } else if (dir == 4) {
-//       if (y > 0) {
-//         y -= speed * timePassed
-//       }
-//     }
+    if (dir == 1) {
+      if (x + 100 < 600) {
+        x += speed * timePassed
+      }
+    } else if (dir == 2) {
+      if (x > 0) {
+        x -= speed * timePassed
+      }
+    } else if (dir == 3) {
+      if (y + 100 < 400) {
+        y += speed * timePassed
+      }
+    } else if (dir == 4) {
+      if (y > 0) {
+        y -= speed * timePassed
+      }
+    }
 
-//     if (
-//       coinx <= x + 100 &&
-//       x <= coinx + 50 &&
-//       coiny <= y + 100 &&
-//       y <= coiny + 50
-//     ) {
-//       score++
-//       coinx = Math.random() * (600 - 50)
-//       coiny = Math.random() * (400 - 50)
-//     }
+    if (
+      coinx <= x + 100 &&
+      x <= coinx + 50 &&
+      coiny <= y + 100 &&
+      y <= coiny + 50
+    ) {
+      score++
+      coinx = Math.random() * (600 - 50)
+      coiny = Math.random() * (400 - 50)
+    }
 
-//     window.requestAnimationFrame(draw)
-//   }
-//   draw()
-// }
+    window.requestAnimationFrame(draw)
+  }
+  draw()
+}
 
 //TODO: Summary (Резюме)
 // Коллекционер монет — это очень простая игра, демонстрирующая, как создавать игры в Интернете.
@@ -2400,57 +2400,57 @@ Your browser does not support the HTML5 canvas tag.
 //   count = 0
 // }
 // Поздравляем, у вас есть полнофункциональная игра! Поэкспериментируйте со скоростью падения и гравитацией, добавьте более сложную логику, настройте свою игру и поделитесь своим творением с сообществом!
-window.onload = function () {
-  let btn = document.getElementById('jump')
-  let count = 0
-  var canvas = document.getElementById('canvas')
-  var context = canvas.getContext('2d')
-  var x = 300
-  var y = 350
+// window.onload = function () {
+//   let btn = document.getElementById('jump')
+//   let count = 0
+//   var canvas = document.getElementById('canvas')
+//   var context = canvas.getContext('2d')
+//   var x = 300
+//   var y = 350
 
-  var t = Date.now()
-  let speed = 25
+//   var t = Date.now()
+//   let speed = 25
 
-  context.fillStyle = 'red'
-  context.fill()
+//   context.fillStyle = 'red'
+//   context.fill()
 
-  document.onkeydown = function () {
-    count += 1
-    y -= 25
-  }
-  document.ontouchstart = function () {
-    count += 1
-    y -= 25
-  }
+//   document.onkeydown = function () {
+//     count += 1
+//     y -= 25
+//   }
+//   document.ontouchstart = function () {
+//     count += 1
+//     y -= 25
+//   }
 
-  function draw() {
-    var timePassed = (Date.now() - t) / 1000
-    t = Date.now()
+//   function draw() {
+//     var timePassed = (Date.now() - t) / 1000
+//     t = Date.now()
 
-    //clearing the canvas (очистка холста)
-    context.clearRect(0, 0, 600, 400)
+//     //clearing the canvas (очистка холста)
+//     context.clearRect(0, 0, 600, 400)
 
-    //redrawing the circle (перерисовывание круга)
-    context.beginPath()
-    context.arc(x, y, 50, 0, 2 * Math.PI)
-    context.fillStyle = 'red'
-    context.fill()
+//     //redrawing the circle (перерисовывание круга)
+//     context.beginPath()
+//     context.arc(x, y, 50, 0, 2 * Math.PI)
+//     context.fillStyle = 'red'
+//     context.fill()
 
-    //drawing the count value (рисование значения счета)
-    context.font = '25px Arial'
-    context.fillStyle = 'white'
-    context.fillText('Count: ' + count, 20, 30)
+//     //drawing the count value (рисование значения счета)
+//     context.font = '25px Arial'
+//     context.fillStyle = 'white'
+//     context.fillText('Count: ' + count, 20, 30)
 
-    if (y <= 350) {
-      speed += 50 * timePassed
-      y += speed * timePassed
-      speed = 25
-    }
-    if (y > 350) {
-      count = 0
-    }
+//     if (y <= 350) {
+//       speed += 50 * timePassed
+//       y += speed * timePassed
+//       speed = 25
+//     }
+//     if (y > 350) {
+//       count = 0
+//     }
 
-    window.requestAnimationFrame(draw)
-  }
-  draw()
-}
+//     window.requestAnimationFrame(draw)
+//   }
+//   draw()
+// }
