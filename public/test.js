80b5ed08e3e23e497fc0d268c3d59ebf18066bad
vitalyvitmens <otm_@tut.by>
2023-02-10 15:50:01 +0300

    add data in project

--- public/test.js
+++ public/test.js
@@ -1,104 +1,29 @@
-// const obj = {
-//   name: 'Vladimir',
-//   age: 52,
-//   job: 'Revolutionary',
-// }
+const IndexArray = new Proxy(Array, {
+  construct(target, [args]) {
+    const index = {}
+    args.forEach((item) => (index[item.id] = item))
 
-// const op = new Proxy(obj, {
-//   get(target, prop) {
-//     // console.log(`Getting prop ${prop}`)
-//     if (!(prop in target)) {
-//       return prop
-//         .split('_')
-//         .map((map_prop) => target[map_prop])
-//         .join(' ')
-//     }
-//     return target[prop]
-//   },
-//   set(target, prop, value) {
-//     if (prop in target) {
-//       target[prop] = value
-//     } else {
-//       throw new Error(`{${prop}} - No field in target`)
-//     }
-//   },
-//   has(target, prop) {
-//     return ['name', 'age', 'job'].includes(prop)
-//     // return Object.keys(target).includes(prop)
-//     // return Object.values(target).includes(prop)
-//   },
-//   deleteProperty(target, prop) {
-//     console.log(`Deleting... ${prop}`)
-//     delete target[prop]
-//     return true
-//   },
-// })
-
-// const log = (text) => `Log: ${text}`
-
-// const fp = new Proxy(log, {
-//   apply(target, thisArg, argArray) {
-//     console.log('Calling fn...')
-
-//     return target.apply(thisArg, argArray).toUpperCase()
-//   },
-// })
-
-// class Person {
-//   constructor(name, age) {
-//     this.name = name
-//     this.age = age
-//   }
-// }
-
-// const PersonProxy = new Proxy(Person, {
-//   construct(target, args) {
-//     console.log('Construct...')
-
-//     return new Proxy(new target(...args), {
-//       get(targ, prop) {
-//         console.log(`Getting prop: '${prop}'`)
-//         return targ[prop]
-//       },
-//       set(targ, args, value) {
-//         if (args in targ) {
-//           targ[args] = value
-//         } else {
-//           throw new Error(`No ${prop} field in target`)
-//         }
-//       },
-//     })
-//   },
-// })
-
-// const p = new PersonProxy('Maxim', 30)
-
-function withDefaultValue(t, dv) {
-  return new Proxy(t, {
-    get(o, p) {
-      return p in o ? o[p] : dv
-    },
-  })
-}
-
-const q = withDefaultValue(
-  {
-    x: 12,
-    y: 24,
+    return new Proxy(new target(...args), {
+      get(arr, prop) {
+        switch (prop) {
+          case 'push':
+            return (item) => {
+              index[item.id] = item
+              arr[prop].call(arr, item)
+            }
+          case 'findById':
+            return (id) => index[id]
+          default:
+            return arr[prop]
+        }
+      },
+    })
   },
-  100
-)
-
-const withHiddenProps = (target, prefix = '_') => {
-  return new Proxy(target, {
-    has: (o, p) => p in o && !p.startsWith(prefix),
-    ownKeys: (o) => Reflect.ownKeys(o).filter((p) => !p.startsWith(prefix)),
-    get: (o, p, r) => (p in r ? o[p] : void 0),
-  })
-}
-
-const a = withHiddenProps({
-  name: 'Vladimir',
-  age: 25,
-  _uid: '123949449',
 })
+
+const users = new IndexArray([
+  { id: 1, name: 'Vladimir', job: 'Revolutionary', age: 53 },
+  { id: 2, name: 'Iosif', job: 'General Secretary', age: 73 },
+  { id: 3, name: 'Lavrentiy', job: 'General Commissioner', age: 54 },
+  { id: 4, name: 'Yuriy', job: 'Spaceman', age: 34 },
+])
